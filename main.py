f1e25c168351bab85bd6fc7dbe5cc6f2ffc18ca0
Frida Ruh <35970446+fridaruh@users.noreply.github.com>
2024-12-17 21:31:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-# Importa las funciones
 from conexion_datos import get_record_by_address
+from agente_v1 import analyze_wallet, fecha_generacion
 
-#solicitar el contact_address
-from_address = input("Ingrese la wallet: ")
+def main():
+    # Solicitar el contact_address
+    from_address = input("Ingrese la wallet: ")
 
-# Ejemplo de uso
-try:
-    # Reemplaza 'nombre_tabla' con el nombre de tu tabla real
-    # y '1' con el ID que quieras consultar
-    resultado = get_record_by_address('tornado', from_address)
-    if resultado:
-        print("Registro encontrado:", resultado)
-    else:
-        print("No se encontró el registro")
-except Exception as e:
-    print(f"Ocurrió un error: {str(e)}")
+    try:
+        ###### Tornado Cash ######
+        resultado = get_record_by_address('tornado', from_address)
+        
+        if resultado:
+            print("Procesando wallet...")
+            # Analizar la wallet usando la función de abejas.py
+            analisis = analyze_wallet(from_address)
+            print("\nAnálisis de la wallet:")
+            print("Generated at: "+ fecha_generacion + "\n" + analisis)
+            
+            
+        else:
+            print("No se encontró el registro en la tabla tornado cash")
+            
+    except Exception as e:
+        print(f"Ocurrió un error: {str(e)}")
+
+if __name__ == "__main__":
+    main()
